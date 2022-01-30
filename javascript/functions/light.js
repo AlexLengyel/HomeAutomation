bc64610b9c0594c0da533f4de7380237038ab08e
Alex Lengyel <lengyelalex@gmail.com>
2022-01-30 16:19:23 +0100

    Add floor plan's background color changing function

--- javascript/functions/light.js
+++ javascript/functions/light.js
@@ -4,23 +4,31 @@ const lightOnClick = (lightId, lightAttributes, roomSvgId) => {
   // Get light's current value
   const currentValue = light.getAttribute("value");
 
-  const { trueValue, falseValue, opacityChangeValue, onImg, offImg } =
+  const { trueValue, falseValue, opacityChangerValue, onImg, offImg } =
     lightAttributes;
   const roomSvg = document.getElementById(roomSvgId);
   const roomOpacityValue = parseFloat(roomSvg.style.fillOpacity);
 
   // Switch to light's opposite state and change the room's opacity value
   if (currentValue === trueValue) {
-    setAttributes(light, {
-      "src": offImg,
-      "value": falseValue,
-    });
-    roomSvg.style.fillOpacity = roomOpacityValue + opacityChangeValue;
+    roomFeatureStateChanger(
+      light,
+      falseValue,
+      offImg,
+      roomSvg,
+      roomOpacityValue,
+      "+",
+      opacityChangerValue
+    );
   } else {
-    setAttributes(light, {
-      "src": onImg,
-      "value": trueValue,
-    });
-    roomSvg.style.fillOpacity = roomOpacityValue - opacityChangeValue;
+    roomFeatureStateChanger(
+      light,
+      trueValue,
+      onImg,
+      roomSvg,
+      roomOpacityValue,
+      "-",
+      opacityChangerValue
+    );
   }
 };
